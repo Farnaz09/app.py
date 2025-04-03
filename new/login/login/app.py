1f96cf8124140041bbafc498bb4082d66a82ef0a
Farnaz09 <91029929+Farnaz09@users.noreply.github.com>
2025-04-03 10:15:08 +0530

    added app.py

---
+++ new/login/login/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+# Configure the Streamlit page
+st.set_page_config(page_title="Data Viewer", layout='wide')
+
+st.title("📊 Data Viewer")
+
+st.write("vdvmdfdhfhhfhdvjjj")
